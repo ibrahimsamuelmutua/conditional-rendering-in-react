cc695894fd92bda38f62960730ade6e61d8cf9c3
Ibrahim Samuel Mutua <samuelmutuaibrahim@gmail.com>
2024-11-04 19:36:52 +0300

    learned conditional rendering of components using react

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import Greeting from './Greetings.jsx'
+function App() {
+
+
+  return (
+    <Greeting
+    isLoggedIn ={true}
+    username = "Samuel"
+    />
+
+
+  )
+}
+
+
+
+
+export default App
---
+++ src/Greetings.jsx
@@ -0,0 +1,33 @@
+import propTypes from 'prop-types'
+
+function Greeting(props){
+
+
+  const loggedIn =
+    <h4 className="logged-in">Welcome, {props.username} </h4>
+  
+  const notLoggedIn =
+  <h4 className="not-logged-in">Please loggin</h4>
+  
+
+
+
+  return(
+
+
+    props.isLoggedIn ? loggedIn:notLoggedIn
+    
+    
+    )
+}
+Greeting.propTypes ={
+  username:propTypes.string,
+  isLoggedIn:propTypes.bool,
+}
+
+Greeting.defaultProps={
+  username: "Guest",
+  isLoggedIn: false,
+}
+
+export default Greeting
